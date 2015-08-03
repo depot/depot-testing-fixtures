4907a29e5e35c3353a07ac2c9eb1c644b932e8ff
Beau Simensen <beau@dflydev.com>
2015-08-03 01:09:00 -0500

    Test commit & fetch.

--- src/Monii/AggregateEventStorage/EventStore/Persistence/PersistenceTest.php
+++ src/Monii/AggregateEventStorage/EventStore/Persistence/PersistenceTest.php
@@ -2,12 +2,14 @@
 
 namespace Monii\AggregateEventStorage\EventStore\Persistence;
 
-use Monii\AggregateEventStorage\EventStore\EventEnvelope;
-use Monii\AggregateEventStorage\Fixtures\Banking\Account\AccountWasOpened;
-use Monii\AggregateEventStorage\Fixtures\Banking\Account\AccountBalanceIncreased;
-use Monii\AggregateEventStorage\Fixtures\Banking\Account\AccountBalanceDecreased;
-use Monii\AggregateEventStorage\EventStore\Persistence\Persistence;
+use Monii\AggregateEventStorage\Contract\ContractResolver;
 use Monii\AggregateEventStorage\Contract\SimplePhpFqcnContractResolver;
+use Monii\AggregateEventStorage\EventStore\EventEnvelope;
+use Monii\AggregateEventStorage\EventStore\Transaction\CommitId;
+use Monii\AggregateEventStorage\Fixtures\Banking\Account\Account;
+use Monii\AggregateEventStorage\Fixtures\Banking\Account\AccountBalanceDecreased;
+use Monii\AggregateEventStorage\Fixtures\Banking\Account\AccountBalanceIncreased;
+use Monii\AggregateEventStorage\Fixtures\Banking\Account\AccountWasOpened;
 use PHPUnit_Framework_TestCase as TestCase;
 
 abstract class PersistenceTest extends TestCase
@@ -25,77 +27,234 @@ abstract class PersistenceTest extends TestCase
     public function setUp()
     {
         parent::setUp();
+
         $this->loadFixtures($this->createPersistence());
     }
 
-    private function setUpContractResolver()
+    private function getContractResolver()
     {
-        $this->contractResolver = new SimplePhpFqcnContractResolver();
+        if (! is_null($this->contractResolver)) {
+            return $this->contractResolver;
+        }
+
+        return $this->contractResolver = new SimplePhpFqcnContractResolver();
     }
 
     /**
      * Create a persistence instance.
      *
-     * For persistence implementations that are query based (basically
-     * anything not InMemoryPersistence) each call to this method
-     * should return a brand new instance. This will allow for
-     * creation of fixtures using one persistence and then
-     * any additional work can be done in a state-free
-     * environment.
+     * This method should cause the implementation to create a new
+     * persistence instance. If there is cached involved, this
+     * method should reset and/or prime the cache.
      *
      * @return Persistence
      */
     abstract protected function createPersistence();
 
+    /**
+     * Get a persistence instance.
+     *
+     * @return Persistence
+     */
+    abstract protected function getPersistence();
+
     protected function createEventEnvelope($eventId, $event)
     {
         return new EventEnvelope(
-            $this->contractResolver->resolveFromObject($event),
+            $this->getContractResolver()->resolveFromObject($event),
             $eventId,
             $event
         );
     }
 
-    protected function loadFixtures()
-    {
-        // create a known set of data
-        $this->fixtures = array(
-            $this->createEventEnvelope(
-                123,
-                new AccountWasOpened('fixture-account-000', 25)
-            ),
-            $this->createEventEnvelope(
-                124,
-                new AccountWasOpened('fixture-account-001', 45)
-            ),
-            $this->createEventEnvelope(
-                125,
-                new AccountBalanceIncreased('fixture-account-000', 105)
-            ),
-            $this->createEventEnvelope(
-                126,
-                new AccountBalanceIncreased('fixture-account-001', 10)
-            ),
-            $this->createEventEnvelope(
-                127,
-                new AccountBalanceDecreased('fixture-account-000', 50)
-            )
+    protected function createCommit(
+        Persistence $persistence,
+        $commitId,
+        $aggregateClassName,
+        $aggregateId,
+        $expectedAggregateVersion,
+        array $eventEnvelopes
+    ) {
+        $persistence->commit(
+            CommitId::fromString($commitId),
+            $this->getContractResolver()->resolveFromClassName($aggregateClassName),
+            $aggregateId,
+            $expectedAggregateVersion,
+            $eventEnvelopes
         );
     }
 
-    public function testFetch(Persistence $persistence)
+    protected function loadFixtures()
     {
-        $this->loadFixtures();
-
-        // Do we need to create a mock object here?
-        //$persistence->fetch();
+        $this->doLoadFixtures($this->createPersistence());
     }
 
-    public function testCommit(Persistence $persistence)
+    protected function doLoadFixtures(Persistence $persistence)
+    {
+        $this->createCommit(
+            $persistence,
+            '4A9F269C-27D5-46C2-9FDF-F7A7D61C55D4',
+            Account::class,
+            'fixture-account-000',
+            0,
+            [
+                $this->createEventEnvelope(
+                    123,
+                    new AccountWasOpened('fixture-account-000', 25)
+                ),
+            ]
+        );
+
+        $this->createCommit(
+            $persistence,
+            '75BCD437-F184-4305-AB61-784761536783',
+            Account::class,
+            'fixture-account-001',
+            0,
+            [
+                $this->createEventEnvelope(
+                    124,
+                    new AccountWasOpened('fixture-account-001', 10)
+                ),
+                $this->createEventEnvelope(
+                    125,
+                    new AccountBalanceIncreased('fixture-account-001', 15)
+                ),
+                $this->createEventEnvelope(
+                    126,
+                    new AccountBalanceDecreased('fixture-account-001', 5)
+                ),
+                $this->createEventEnvelope(
+                    127,
+                    new AccountBalanceIncreased('fixture-account-001', 45)
+                ),
+            ]
+        );
+
+        $this->createCommit(
+            $persistence,
+            '1264416A-7465-4241-A810-B5EFBD1988E2',
+            Account::class,
+            'fixture-account-000',
+            1,
+            [
+                $this->createEventEnvelope(
+                    128,
+                    new AccountBalanceIncreased('fixture-account-000', 30)
+                ),
+            ]
+        );
+
+        $this->createCommit(
+            $persistence,
+            'D68A5BFD-6A61-44A7-BF10-ECEFE776A141',
+            Account::class,
+            'fixture-account-001',
+            4,
+            [
+                $this->createEventEnvelope(
+                    129,
+                    new AccountBalanceDecreased('fixture-account-001', 75)
+                ),
+                $this->createEventEnvelope(
+                    130,
+                    new AccountBalanceIncreased('fixture-account-001', 90)
+                ),
+            ]
+        );
+
+        $this->createCommit(
+            $persistence,
+            'A8DA72AB-1405-463A-AF16-BF170A5D304E',
+            Account::class,
+            'fixture-account-001',
+            6,
+            [
+                $this->createEventEnvelope(
+                    131,
+                    new AccountBalanceIncreased('fixture-account-001', 125)
+                ),
+                $this->createEventEnvelope(
+                    132,
+                    new AccountBalanceDecreased('fixture-account-001', 15)
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @param $aggregateClassName
+     * @param $aggregateId
+     * @param $expectedEventEnvelopes
+     * @dataProvider provideFetchData
+     */
+    public function testFetch($aggregateClassName, $aggregateId, $expectedEventEnvelopes)
     {
         $this->loadFixtures();
 
-        // Do we need to create a mock object here?
-        //$persistence->commit($commitId);
+        $actualEventEnvelopes = $this->getPersistence()->fetch(
+            $this->getContractResolver()->resolveFromClassName($aggregateClassName),
+            $aggregateId
+        );
+
+        $this->assertEquals($expectedEventEnvelopes, $actualEventEnvelopes);
+    }
+
+    public function provideFetchData()
+    {
+        return [
+            [
+                Account::class,
+                'fixture-account-000',
+                [
+                    $this->createEventEnvelope(
+                        123,
+                        new AccountWasOpened('fixture-account-000', 25)
+                    ),
+                    $this->createEventEnvelope(
+                        128,
+                        new AccountBalanceIncreased('fixture-account-000', 30)
+                    ),
+                ],
+            ],
+            [
+                Account::class,
+                'fixture-account-001',
+                [
+                    $this->createEventEnvelope(
+                        124,
+                        new AccountWasOpened('fixture-account-001', 10)
+                    ),
+                    $this->createEventEnvelope(
+                        125,
+                        new AccountBalanceIncreased('fixture-account-001', 15)
+                    ),
+                    $this->createEventEnvelope(
+                        126,
+                        new AccountBalanceDecreased('fixture-account-001', 5)
+                    ),
+                    $this->createEventEnvelope(
+                        127,
+                        new AccountBalanceIncreased('fixture-account-001', 45)
+                    ),
+                    $this->createEventEnvelope(
+                        129,
+                        new AccountBalanceDecreased('fixture-account-001', 75)
+                    ),
+                    $this->createEventEnvelope(
+                        130,
+                        new AccountBalanceIncreased('fixture-account-001', 90)
+                    ),
+                    $this->createEventEnvelope(
+                        131,
+                        new AccountBalanceIncreased('fixture-account-001', 125)
+                    ),
+                    $this->createEventEnvelope(
+                        132,
+                        new AccountBalanceDecreased('fixture-account-001', 15)
+                    ),
+                ],
+            ],
+        ];
     }
 }
